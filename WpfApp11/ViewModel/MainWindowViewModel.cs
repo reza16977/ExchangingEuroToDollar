30d5e181bc3be7e9a75a96f408d2e7584c162d46
82106 <reza16977@gmail.com>
2023-06-12 19:36:36 +0900

    Add project files.

---
+++ WpfApp11/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp11
+{
+    
+     class MainWindowViewModel : ViewModelBase
+    {
+        private double euros;
+        public double Euros
+        {
+            get { return euros; }
+            set { euros = value;
+                OnPropertyChanged("Euros");
+                OnEurosChanged();
+            }
+        }
+
+        private double dollars;
+        public double Dollars
+        {
+            get { return dollars; }
+            set
+            {
+                dollars = value;
+                OnPropertyChanged("Dollars");
+            }
+        }
+
+        private void OnEurosChanged()
+        {
+            Dollars = Math.Round(Euros * 1.1, 2);
+        }
+
+
+       
+    }
+}
---
+++ WpfApp11/ViewModel/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp11
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
